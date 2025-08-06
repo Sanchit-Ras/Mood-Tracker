92d91417c526526690536495ed126a4e3366710b
Sanchit-Ras <sanchitz123rastogi@gmail.com>
2025-08-07 00:47:02 +0530

    Initial Commit

---
+++ src/HappyIncrementor.tsx
@@ -0,0 +1,20 @@
+import {type FC} from 'react';
+import { useDispatch } from 'react-redux';
+import { happyButtonClicked } from './actions';
+
+type HappyIncrementorProps={
+
+}
+const HappyIncrementor:FC<HappyIncrementorProps>=(props)=>{
+    const dispatch=useDispatch();
+    function increment(){
+        dispatch(happyButtonClicked);
+    }
+    return(
+        <div className='m-4 font-bold'>
+            <h3>Are you happy?</h3>
+            <button className='bg-amber-400 p-3' onClick={increment}>Yes</button>
+        </div>
+    );
+}
+export default HappyIncrementor;
